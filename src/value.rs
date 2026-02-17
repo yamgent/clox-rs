172c2580c87f4e619a01ccb063e4a82ef2b2305d
Maurice Wangleng Tan <tanwangleng@outlook.com>
2026-02-17 09:17:22 +0800

    chore: add tests for values.rs

--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,6 @@
 pub type Value = f64;
 
+#[derive(Debug, PartialEq)]
 pub struct ValueArray {
     values: Vec<Value>,
 }
@@ -16,5 +17,29 @@ impl ValueArray {
 
     pub fn get(&self, i: usize) -> Value {
         self.values[i]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut value_array = ValueArray::new();
+        assert_eq!(value_array.add(7.0), 0);
+        assert_eq!(value_array.add(5.5), 1);
+        assert_eq!(value_array.add(9.0), 2);
+        assert_eq!(value_array.values, vec![7.0, 5.5, 9.0]);
+    }
+
+    #[test]
+    fn test_get() {
+        let value_array = ValueArray {
+            values: vec![7.0, 5.5, 9.0],
+        };
+        assert_eq!(value_array.get(0), 7.0);
+        assert_eq!(value_array.get(1), 5.5);
+        assert_eq!(value_array.get(2), 9.0);
     }
 }
