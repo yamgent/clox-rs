5069b10fff4bfc333098f2c4d4dcce649912b5c4
Maurice Wangleng Tan <tanwangleng@outlook.com>
2026-02-21 14:05:29 +0800

    chore: add tests for value.rs

--- src/value.rs
+++ src/value.rs
@@ -36,24 +36,57 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
+    fn test_value_is_falsey() {
+        // in Lox, only nil and false is falsey, everything else is true (even 0!)
+        assert!(Value::Nil.is_falsey());
+        assert!(Value::Bool(false).is_falsey());
+
+        assert!(!Value::Bool(true).is_falsey());
+        assert!(!Value::Number(0.0).is_falsey());
+        assert!(!Value::Number(1.0).is_falsey());
+        assert!(!Value::Number(-1.0).is_falsey());
+        assert!(!Value::Number(0.5).is_falsey());
+    }
+
+    #[test]
+    fn test_value_array_add() {
         let mut value_array = ValueArray::new();
         assert_eq!(value_array.add(Value::Number(7.0)), 0);
         assert_eq!(value_array.add(Value::Number(5.5)), 1);
         assert_eq!(value_array.add(Value::Number(9.0)), 2);
+        assert_eq!(value_array.add(Value::Nil), 3);
+        assert_eq!(value_array.add(Value::Bool(true)), 4);
+        assert_eq!(value_array.add(Value::Bool(false)), 5);
         assert_eq!(
             value_array.values,
-            vec![Value::Number(7.0), Value::Number(5.5), Value::Number(9.0)]
+            vec![
+                Value::Number(7.0),
+                Value::Number(5.5),
+                Value::Number(9.0),
+                Value::Nil,
+                Value::Bool(true),
+                Value::Bool(false)
+            ]
         );
     }
 
     #[test]
-    fn test_get() {
+    fn test_value_array_get() {
         let value_array = ValueArray {
-            values: vec![Value::Number(7.0), Value::Number(5.5), Value::Number(9.0)],
+            values: vec![
+                Value::Number(7.0),
+                Value::Number(5.5),
+                Value::Number(9.0),
+                Value::Nil,
+                Value::Bool(true),
+                Value::Bool(false),
+            ],
         };
         assert_eq!(value_array.get(0), Value::Number(7.0));
         assert_eq!(value_array.get(1), Value::Number(5.5));
         assert_eq!(value_array.get(2), Value::Number(9.0));
+        assert_eq!(value_array.get(3), Value::Nil);
+        assert_eq!(value_array.get(4), Value::Bool(true));
+        assert_eq!(value_array.get(5), Value::Bool(false));
     }
 }
